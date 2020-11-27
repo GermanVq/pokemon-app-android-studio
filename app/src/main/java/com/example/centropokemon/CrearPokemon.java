9ce299e6c16cd30ba7d7313ac18424e0c80e2e3d
GermanVq <gavegaq@gmail.com>
2020-11-27 11:06:39 -0500

    item pokemon and entrenador create

---
+++ app/src/main/java/com/example/centropokemon/CrearPokemon.java
@@ -0,0 +1,14 @@
+package com.example.centropokemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPokemon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_pokemon);
+    }
+}
