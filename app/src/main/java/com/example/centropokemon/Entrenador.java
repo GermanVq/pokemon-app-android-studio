d474cec65797c3357b5d1a18d0b18df322149015
GermanVq <gavegaq@gmail.com>
2020-11-27 12:39:52 -0500

    CrearEntrenador aded

--- app/src/main/java/com/example/centropokemon/Entrenador.java
+++ app/src/main/java/com/example/centropokemon/Entrenador.java
@@ -6,6 +6,16 @@ public class Entrenador {
     private String nombre;
     private String edad;
     private String ciudad;
+    private String sexo;
+
+    public Entrenador(int foto, String id, String nombre, String edad, String ciudad, String sexo) {
+        this.foto = foto;
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ciudad = ciudad;
+        this.sexo = sexo;
+    }
 
     public int getFoto() {
         return foto;
@@ -47,13 +57,14 @@ public class Entrenador {
         this.ciudad = ciudad;
     }
 
-    public Entrenador(int foto, String id, String nombre, String edad, String ciudad) {
-        this.foto = foto;
-        this.id = id;
-        this.nombre = nombre;
-        this.edad = edad;
-        this.ciudad = ciudad;
+    public String getSexo() {
+        return sexo;
     }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
     public  void  guardarEn(){ Datos.guardarEn(this);}
 
     public void  eliminarEn(){
