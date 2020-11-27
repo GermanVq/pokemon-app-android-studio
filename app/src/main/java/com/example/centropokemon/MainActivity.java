3c1e243d96d63c7610d01dc784dc5e85e7c30f08
GermanVq <gavegaq@gmail.com>
2020-11-27 09:41:02 -0500

    SplashScreen aded

--- app/src/main/java/com/example/centropokemon/MainActivity.java
+++ app/src/main/java/com/example/centropokemon/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.centropokemon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( MainActivity.this, InicioActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 2500);
     }
+
 }
