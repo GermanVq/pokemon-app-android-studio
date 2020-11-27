6e75c309cc2038cec8819ae621697773d90bd66f
GermanVq <gavegaq@gmail.com>
2020-11-27 12:20:17 -0500

    interface pokemon register aded

--- app/src/main/java/com/example/centropokemon/InicioActivity.java
+++ app/src/main/java/com/example/centropokemon/InicioActivity.java
@@ -2,13 +2,34 @@ package com.example.centropokemon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionMenu;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class InicioActivity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+
+
+
     }
+
+    public void AgregarPokemon(View v){
+        intent = new Intent(InicioActivity.this, CrearPokemon.class);
+        startActivity(intent);
+    }
+    public void AgregarEntrenador(View v){
+        intent = new Intent(InicioActivity.this, CrearEntrenador.class);
+        startActivity(intent);
+    }
+
+
 }
